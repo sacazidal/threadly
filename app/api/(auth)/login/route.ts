481fd861b7b00c399d89211fda16705f78227ed2
sacazidal <tw1nkle001@yandex.ru>
2025-03-24 00:17:29 +0700

    add base

--- app/api/(auth)/login/route.ts
+++ app/api/(auth)/login/route.ts
@@ -1,9 +1,21 @@
+import { validationLog } from "@/utils/validation";
 import { NextRequest, NextResponse } from "next/server";
+
+interface Request {
+  email: string;
+  passwordHash: string;
+}
 
 export async function POST(request: NextRequest) {
   try {
     // Получаем тело запроса
-    const { email, passwordHash } = await request.json();
+    const { email, passwordHash }: Request = await request.json();
+
+    // проверяю, что все поля заполнены и прошли валидацию
+    const validationError = validationLog(email, passwordHash);
+    if (validationError) {
+      return NextResponse.json({ error: validationError }, { status: 400 });
+    }
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
--- app/api/(auth)/recovery/route.ts
+++ app/api/(auth)/recovery/route.ts
@@ -1,3 +1,22 @@
-import { NextRequest } from "next/server";
+import { validationRecovery } from "@/utils/validation";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {}
+interface Request {
+  email: string;
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    // Получаем тело запроса
+    const { email }: Request = await request.json();
+
+    // проверяю, что все поля заполнены и прошли валидацию
+    const validationError = validationRecovery(email);
+    if (validationError) {
+      return NextResponse.json({ error: validationError }, { status: 400 });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
+  }
+}
