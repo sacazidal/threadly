da54d72321aa843a3993c41f2c3972a3cb696c83
sacazidal <tw1nkle001@yandex.ru>
2025-03-24 20:40:58 +0700

    fix deploy 0.6

--- app/api/(sale)/product/[id]/route.ts
+++ app/api/(sale)/product/[id]/route.ts
@@ -1,5 +1,7 @@
+import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+
+const prisma = new PrismaClient();
 
 export async function GET(
   request: NextRequest,
