a6a440b6dbc99833dd738383edbbe045aa141146
sacazidal <tw1nkle001@yandex.ru>
2025-03-24 22:06:18 +0700

    fix prod vers

--- utils/fetchUrl.ts
+++ utils/fetchUrl.ts
@@ -1,29 +1,29 @@
 export const fetchUrlLogout =
   process.env.NODE_ENV === "production"
-    ? "/api/logout"
+    ? "https://threadly-three.vercel.app/api/logout"
     : "http://localhost:3000/api/logout";
 
 export const fetchUrlLogin =
   process.env.NODE_ENV === "production"
-    ? "/api/login"
+    ? "https://threadly-three.vercel.app/api/login"
     : "http://localhost:3000/api/login";
 
 export const fetchUrlRegister =
   process.env.NODE_ENV === "production"
-    ? "/api/signup"
+    ? "https://threadly-three.vercel.app/api/signup"
     : "http://localhost:3000/api/signup";
 
 export const fetchUrlRecovery =
   process.env.NODE_ENV === "production"
-    ? "/api/recovery"
+    ? "https://threadly-three.vercel.app/api/recovery"
     : "http://localhost:3000/api/recovery";
 
 export const fetchUrlSearch =
   process.env.NODE_ENV === "production"
-    ? "/api/search"
+    ? "https://threadly-three.vercel.app/api/search"
     : "http://localhost:3000/api/search";
 
 export const fetchUrlById =
   process.env.NODE_ENV === "production"
-    ? "/api/product"
+    ? "https://threadly-three.vercel.app/api/product"
     : "http://localhost:3000/api/product";
