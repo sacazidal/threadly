c2f69f913b75b5d4bd629009ba5dee814a52d998
sacazidal <tw1nkle001@yandex.ru>
2025-03-25 22:03:50 +0700

    create handleClick => push(href)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,20 @@
+"use client";
+
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
 import { actions } from "@/constants/userActions";
 import UserActions from "./UserActions";
+import { useRouter } from "next/navigation";
 
 const Header = () => {
+  const { push } = useRouter();
+
+  const handleActionClick = (href: string) => {
+    if (href) {
+      push(href);
+    }
+  };
+
   return (
     <header className="py-2 md:py-5 px-3">
       <div className="max-w-screen-2xl mx-auto flex md:flex-nowrap gap-y-2 flex-wrap justify-between items-center gap-x-3">
@@ -16,6 +27,7 @@ const Header = () => {
             <UserActions
               key={action.id}
               icon={action.Icon}
+              onClick={() => handleActionClick(action.href)}
               label={action.label}
             />
           ))}
