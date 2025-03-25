a8e7b610c8aaf9c93e824cda0f8dcd69d61e8fb9
sacazidal <tw1nkle001@yandex.ru>
2025-03-25 22:10:00 +0700

    fix style for dark theme

--- components/TakeDiscount.tsx
+++ components/TakeDiscount.tsx
@@ -1,6 +1,6 @@
 const TakeDiscount = () => {
   return (
-    <button className="w-full text-xs md:rounded-b-2xl bg-amber-700 hover:bg-amber-800 transition-colors duration-300 py-1 md:py-2 px-3 cursor-pointer">
+    <button className="w-full text-xs text-neutral-50 md:rounded-b-2xl bg-amber-700 hover:bg-amber-800 transition-colors duration-300 py-1 md:py-2 px-3 cursor-pointer">
       Получить скидку
     </button>
   );
