7113cae10bab5a866bd0ae3263035cddd6058c30
sacazidal <tw1nkle001@yandex.ru>
2025-03-25 22:12:30 +0700

    add comp Trans

--- components/Translate.tsx
+++ components/Translate.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
+import ToggleTheme from "./toggle-theme";
 
 const Translate = () => {
   return (
-    <>
-      <div className="text-xs">RU</div>
-      <Image
-        src={"/images/locales/ru.webp"}
-        alt="ru"
-        width={20}
-        height={20}
-        className="h-auto w-auto"
-      />
-    </>
+    <div className="flex items-center gap-x-5">
+      <div className="flex gap-x-1">
+        <div className="text-xs">RU</div>
+        <Image
+          src={"/images/locales/ru.webp"}
+          alt="ru"
+          width={20}
+          height={20}
+          className="h-auto w-auto"
+        />
+      </div>
+      <ToggleTheme />
+    </div>
   );
 };
 export default Translate;
