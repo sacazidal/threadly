c5391b2908b49f7376ffe5875802b0b441cac054
sacazidal <tw1nkle001@yandex.ru>
2025-03-25 22:05:00 +0700

    uselessly

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,17 +1,19 @@
-import { navigations } from "@/constants/navigation";
+import { navigation } from "@/constants/navigation";
 import Link from "next/link";
 
 const Navigation = () => {
   return (
     <div className="py-2 px-3">
-      <nav className="max-w-screen-2xl mx-auto hidden md:flex items-center gap-x-8 flex-wrap">
-        {navigations.map((nav) => (
+      <nav className="max-w-screen-2xl mx-auto hidden md:flex items-center gap-x-4 flex-wrap">
+        {navigation.map((nav) => (
           <Link
             key={nav.id}
             href={nav.href}
-            className="flex gap-x-1 items-center"
+            className="flex first-line::bg-amber-100 gap-x-1 items-center px-3 py-1 rounded-md bg-neutral-200"
           >
-            <span className="text-xs">{nav.label}</span>
+            <span className="text-xs text-neutral-900 font-medium">
+              {nav.label}
+            </span>
           </Link>
         ))}
       </nav>
