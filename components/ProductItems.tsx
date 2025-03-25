f708108a72ddd4ff6df19d703c50f7c9dccbaf06
sacazidal <tw1nkle001@yandex.ru>
2025-03-25 22:07:22 +0700

    create comp for mapping Product in Page. read db and conclusion grid

---
+++ components/ProductItems.tsx
@@ -0,0 +1,30 @@
+import prisma from "@/lib/prisma";
+import ProductCard from "./ProductCard";
+
+async function getProducts() {
+  const products = await prisma.product.findMany({
+    select: {
+      id: true,
+      name: true,
+      price: true,
+      description: true,
+      imageUrl: true,
+      category: true,
+    },
+    orderBy: { createdAt: "desc" },
+    take: 12,
+  });
+  return products;
+}
+
+const ProductItems = async () => {
+  const products = await getProducts();
+  return (
+    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
+export default ProductItems;
