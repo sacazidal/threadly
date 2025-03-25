e9c48ff284527aa09548b40868a8dbc22a086463
sacazidal <tw1nkle001@yandex.ru>
2025-03-25 22:13:07 +0700

    added a handler

--- components/UserActions.tsx
+++ components/UserActions.tsx
@@ -1,14 +1,11 @@
-type IconProps = React.ComponentType<{ className?: string }>;
+import { UserActionsProps } from "@/types";
 
-interface UserActionsProps {
-  icon: IconProps;
-  label: string;
-  onClick?: () => void;
-}
-
-const UserActions = ({ icon: Icon, label }: UserActionsProps) => {
+const UserActions = ({ icon: Icon, label, onClick }: UserActionsProps) => {
   return (
-    <button className="flex flex-col justify-center items-center cursor-pointer">
+    <button
+      onClick={onClick}
+      className="flex flex-col justify-center items-center cursor-pointer"
+    >
       <Icon className="md:w-5 md:h-5" />
       <span className="text-xs md:block hidden">{label}</span>
     </button>
