c07447f02d48fd4496d52c3d15ce5d7645d17bfb
sacazidal <tw1nkle001@yandex.ru>
2025-03-27 23:29:51 +0700

    add new route

--- lib/api.ts
+++ lib/api.ts
@@ -2,9 +2,10 @@ const isProduction = process.env.NODE_ENV === "production";
 
 export const API_BASE_URL = isProduction
   ? `https://threadly-three.vercel.app`
-  : "http://localhost:3000";
+  : "http://localhost:3001";
 
 export const apiRoutes = {
   product: `${API_BASE_URL}/api/product`,
   search: `${API_BASE_URL}/api/search`,
+  sendEmail: `${API_BASE_URL}/api/send-email`,
 };
