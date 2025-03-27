9d4aad6531cb04716c2896d9003467fd8d25d23d
sacazidal <tw1nkle001@yandex.ru>
2025-03-27 23:36:01 +0700

    created the api route with the POST method. Checks the email validation, connects to my mail, checks if there is such an email in the DB, if not, then successfully and sends the email, if not, then an error

---
+++ app/api/send-email/route.ts
@@ -0,0 +1,64 @@
+import prisma from "@/lib/prisma";
+import { validateEmail } from "@/utils/validation";
+import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+interface SendEmailRequest {
+  email: string;
+}
+
+export async function POST(request: NextRequest) {
+  const { email }: SendEmailRequest = await request.json();
+
+  if (validateEmail(email)) {
+    return NextResponse.json({ error: "Некорректный email" }, { status: 400 });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.mail.ru",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER!,
+        pass: process.env.EMAIL_PASSWORD!,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER!,
+      to: email,
+      subject: "Threadly. Скидка 10% на первый заказ",
+      text: "Ваш промокод - GLORY2025",
+    };
+
+    const existingEmail = await prisma.mailing.findMany({
+      where: { email },
+    });
+
+    if (existingEmail.length > 0) {
+      return NextResponse.json(
+        { error: "Вы уже подписаны на рассылку" },
+        { status: 400 }
+      );
+    }
+
+    await prisma.mailing.create({
+      data: {
+        email,
+      },
+    });
+
+    await transporter.sendMail(mailOptions);
+
+    return NextResponse.json(
+      { message: "Успешно подписались на рассылку" },
+      { status: 200 }
+    );
+  } catch {
+    return NextResponse.json(
+      { error: "Ошибка при отправке сообщения" },
+      { status: 500 }
+    );
+  }
+}
