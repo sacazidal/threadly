cc360acaacfd12cdf927baaa21a4b838517ea968
sacazidal <tw1nkle001@yandex.ru>
2025-03-30 02:46:40 +0700

    new type and interface for comp

--- types/index.ts
+++ types/index.ts
@@ -29,3 +29,47 @@ export interface FooterMenuProps {
   children: React.ReactNode;
   title: string;
 }
+
+export interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+  title?: string;
+}
+
+export interface SwitchFormProps {
+  onSwitch?: () => void;
+  onRecovery?: () => void;
+  onBackToLogin?: () => void;
+  onBackToSignUp?: () => void;
+  onSuccess?: () => void;
+}
+
+export interface AuthFormProps {
+  title: string;
+  action: string;
+  secondAction: string;
+  onSubmit: (e: React.FormEvent) => void;
+  children: React.ReactNode;
+  footerText: React.ReactNode;
+  loading: boolean;
+  error: string;
+}
+
+export interface FieldFormProps {
+  label: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement>;
+  type: string;
+  id: string;
+  placeholder?: string;
+  recovery?: React.ReactNode;
+}
+
+export type AuthModalType = "login" | "register" | "recovery";
+
+export interface AuthModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  initialForm?: AuthModalType;
+}
