eaaabf319920cd683c0f0f9ec0470d6599d75288
sacazidal <tw1nkle001@yandex.ru>
2025-03-30 22:24:41 +0700

    created the first step of password recovery

--- app/api/(auth)/recovery/route.ts
+++ app/api/(auth)/recovery/route.ts
@@ -1,3 +1,6 @@
+import prisma from "@/lib/prisma";
+import { generateConfirmationEmail } from "@/utils/generateConfirmationEmail";
+import { sendRecoveryCode } from "@/utils/recoveryAccount";
 import { validationRecovery } from "@/utils/validation";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -15,6 +18,34 @@ export async function POST(request: NextRequest) {
     if (validationError) {
       return NextResponse.json({ error: validationError }, { status: 400 });
     }
+
+    const existingUser = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        { error: "Пользователь с таким email не найден" },
+        { status: 404 }
+      );
+    }
+
+    const generateCode = generateConfirmationEmail();
+    await sendRecoveryCode(email, generateCode);
+
+    await prisma.recovery.create({
+      data: {
+        email,
+        userId: Number(existingUser.id),
+        code: generateCode,
+        expiresAt: new Date(Date.now() + 60 * 1000 * 5), // 5 минут
+      },
+    });
+
+    return NextResponse.json(
+      { message: "Инструкция направлена на вашу почту" },
+      { status: 200 }
+    );
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
