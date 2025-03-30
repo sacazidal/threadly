58b4b500db8d19e4ab9474ecf942385517cf7001
sacazidal <tw1nkle001@yandex.ru>
2025-03-30 22:27:10 +0700

    I made a fully functional login with the assignment of a jwt token

--- app/api/(auth)/login/route.ts
+++ app/api/(auth)/login/route.ts
@@ -1,21 +1,74 @@
+import prisma from "@/lib/prisma";
 import { validationLog } from "@/utils/validation";
 import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-interface Request {
+interface LoginRequest {
   email: string;
-  passwordHash: string;
+  password: string;
 }
 
 export async function POST(request: NextRequest) {
-  try {
-    // Получаем тело запроса
-    const { email, passwordHash }: Request = await request.json();
+  // Получаем тело запроса
+  const { email, password }: LoginRequest = await request.json();
 
-    // проверяю, что все поля заполнены и прошли валидацию
-    const validationError = validationLog(email, passwordHash);
-    if (validationError) {
-      return NextResponse.json({ error: validationError }, { status: 400 });
+  // проверяю, что все поля заполнены и прошли валидацию
+  const validationError = validationLog(email, password);
+  if (validationError) {
+    return NextResponse.json({ error: validationError }, { status: 400 });
+  }
+
+  try {
+    const existingUser = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (!existingUser) {
+      return NextResponse.json(
+        { error: "Неверный логин или пароль" },
+        { status: 400 }
+      );
     }
+
+    const passwordMatch = await bcrypt.compare(
+      password,
+      existingUser.passwordHash
+    );
+
+    if (!passwordMatch) {
+      return NextResponse.json(
+        { error: "Неверный логин или пароль" },
+        { status: 400 }
+      );
+    }
+
+    const token = jwt.sign(
+      {
+        userId: existingUser.id,
+      },
+      process.env.JWT_SECRET!,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    const response = NextResponse.json(
+      {
+        message: "Успешная авторизация",
+      },
+      { status: 200 }
+    );
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 60 * 60 * 24 * 7,
+      path: "/",
+      sameSite: "strict",
+    });
+
+    return response;
   } catch (error) {
     console.error(error);
     return NextResponse.json({ error: "Ошибка сервера" }, { status: 500 });
